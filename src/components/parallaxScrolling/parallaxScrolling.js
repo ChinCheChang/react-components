7e96329e6a523f00c95583d6679d2b0ff567f084
ChinCheChang <36709019+ChinCheChang@users.noreply.github.com>
2018-10-31 02:16:21 +0800

    add calender icon

--- src/components/parallaxScrolling/parallaxScrolling.js
+++ src/components/parallaxScrolling/parallaxScrolling.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './parallaxScrolling.css';
 
 const ParallaxScrolling = () => {
+  const showCalender = () => {
+    console.log('showCalender');
+  }
   return (
     <div className="parallax">
       <div className="parallax-img"></div>
+      <i onClick={showCalender} className="far fa-calendar-alt calendarIcon grow"></i>
       <div id="clock">h1</div>
       <div className="br-pill pl2 pr2" id='photoby'>
         <a href='https://unsplash.com/@danesduet10?utm_medium=referral&utm_campaign=photographer-credit&utm_content=creditBadge'
